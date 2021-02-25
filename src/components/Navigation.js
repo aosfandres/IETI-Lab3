a858bee7ab161adeef12fa8e832acc6bde24e964
Andres sotelo <andres.sotelo@mail.escuelaing.edu.co>
2021-02-25 14:05:59 -0500

    despligue ok

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -16,8 +16,6 @@ import ListItem from '@material-ui/core/ListItem';
 import PersonIcon from '@material-ui/icons/Person';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 import Task from './Task';
 import Container from '@material-ui/core/Container';
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Login from './components/Login';
-import Navigation from './components/Navigation';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
